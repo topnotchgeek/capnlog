49c63521f75c93c90dcf0bb5d6a8e6e9e4c83bc7
Jef Duncan <jef@topnotchgeek.com>
2017-02-16 07:30:54 -0800

    webcam support

--- apps/www/viewsets.py
+++ apps/www/viewsets.py
@@ -2,6 +2,7 @@ import json
 import os
 import logging
 from datetime import datetime
+from shutil import copyfile
 
 from django.contrib.auth.models import User, Group
 from django.http.response import JsonResponse, HttpResponseBadRequest, HttpResponse
@@ -9,6 +10,7 @@ from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 
+from conf import settings
 from .serializers import UserSerializer, GroupSerializer, EntrySerializer, TempHumSerializer, SnapshotSerializer, \
     WebcamSerializer, StationSerializer
 from .models import BlogEntry, TempHumidity, Webcam, Snapshot, Station
@@ -69,6 +71,7 @@ class WebcamViewSet(viewsets.ModelViewSet):
         try:
             ss = wc.create_snap(json.loads(request.body))
             if ss:
+                self.make_latest(ss)
                 return JsonResponse(SnapshotSerializer(instance=ss, context={'request': request}).data, status=201)    #{"result": "success", "webcam_id": wc.slug, "img_id": ss.id}
         except ValueError:
             pass
@@ -84,20 +87,36 @@ class WebcamViewSet(viewsets.ModelViewSet):
         wc = self.get_object()
         tz = timezone.get_current_timezone()
         ct = timezone.make_aware(datetime.now(), tz)
+        cnt = False
         try:
             y = int(request.query_params.get('y', ct.year))
             m = int(request.query_params.get('m', ct.month))
             d = int(request.query_params.get('d', ct.day))
             h = int(request.query_params.get('h', -1))
+            cnt = len(request.query_params.get('count', '')) > 0
+
             if h >= 0:
                 sd = timezone.make_aware(datetime(y, m, d, h, 0, 0),tz)
                 ed = timezone.make_aware(datetime(y, m, d, h, 59, 59),tz)
             else:
                 sd = timezone.make_aware(datetime(y, m, d, 0, 0, 0),tz)
                 ed = timezone.make_aware(datetime(y, m, d, 23, 59, 59),tz)
+            if cnt:
+                return JsonResponse(data={'count': wc.snapshot_set.filter(ts_create__range=(sd,ed)).count()})
             return JsonResponse(data=SnapshotSerializer(wc.snapshot_set.filter(ts_create__range=(sd,ed)), many=True, context={'request': request}).data, safe=False)
         except ValueError, e:
             return HttpResponseBadRequest(reason=e.message)
+
+    def make_latest(self, ss):
+        ss = Snapshot()
+        sfnm = ss.img_name
+        sdir = os.path.join(settings.WEBCAM_IMAGE_PATH, ss.img_path)
+        src = os.path.join(sdir, sfnm)
+        dfnm = 'latest.jpg'
+        ddir = settings.WEBCAM_IMAGE_PATH
+        dst = os.path.join(ddir, dfnm)
+        copyfile(src, dst)
+        logger.debug('make_latest: copied %s to %s' % (src, dst))
 
 
 class SnapshotViewSet(viewsets.ModelViewSet):
